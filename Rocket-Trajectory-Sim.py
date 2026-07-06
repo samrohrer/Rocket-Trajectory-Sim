23ba53b2f0b9fad8a049b89580d9da82e771cefe
Sam Rohrer <samueljrohrer@gmail.com>
2026-07-06 13:51:59 -0400

    Step C: Drag equation added in

--- Rocket-Trajectory-Sim.py
+++ Rocket-Trajectory-Sim.py
@@ -8,18 +8,28 @@ time = 0
 mass = 34
 dt = 0.1
 g = -9.81
+drag_coefficient = 0.5  # Cd
+cross_sec_area = 0.0193  # m^2
+density_rho = 1.225  # kg/m^3
 #Lists of Altitudes and Times for every step
 altitude_list = []
 time_list = []
 
-#While Loop for velocity and altitude
+#While loop calculating altitude
 while altitude >= 0:
     burn_time = 3.9
+    #If statement determining whether thrust is on or off
     if time < burn_time:
         thrust = 2500
     else:
         thrust = 0
-    acceleration = (thrust + mass*g)/mass
+    #Drag equation added as a force in acceleration
+    drag_equation = drag_coefficient * density_rho * velocity ** 2 / 2 * cross_sec_area
+    if velocity > 0:
+        drag_equation = -drag_equation
+    else:
+        drag_equation = +drag_equation
+    acceleration = (thrust + drag_equation + mass*g)/mass
     new_velocity = velocity + acceleration * dt
     new_altitude = altitude + new_velocity * dt
     time = time + dt
@@ -33,4 +43,3 @@ plt.plot(time_list, altitude_list)
 plt.xlabel("Time (s)")
 plt.ylabel("Altitude (m)")
 plt.show()
-
