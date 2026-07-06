f8634217e1b76528f10c5efa66a22d59ef9855aa
Sam Rohrer <samueljrohrer@gmail.com>
2026-07-06 16:55:48 -0400

    Step F: Final Organization of Simulation at current state

--- Rocket-Trajectory-Sim.py
+++ Rocket-Trajectory-Sim.py
@@ -13,22 +13,22 @@ drag_coefficient = 0.5  #Cd
 cross_sec_area = 0.0193  #m^2
 density_sea_level = 1.225  #kg/m^3
 atmospheric_scale_height = 8500 #m
-#Lists of Altitudes and Times for every step
-altitude_list = []
-time_list = []
 
 def calculate_thrust(time, burn_time):
+    """Calculates the thrust of the flight simulation."""
     if time < burn_time:
         thrust = 2500
     else:
         thrust = 0
     return thrust
 
-def calculate_density(density_sea_level, altitude, atmosphere_scale_height):
+def calculate_density(density_sea_level, altitude, atmospheric_scale_height):
+    """Calculates the density of the flight simulation."""
     density = density_sea_level * np.exp(-altitude / atmospheric_scale_height)
     return density
 
 def calculate_drag(drag_coefficient, density, velocity, cross_sec_area):
+    """Calculates the drag of the flight simulation."""
     drag = drag_coefficient * density * velocity ** 2 / 2 * cross_sec_area
     if velocity > 0:
         drag = -drag
@@ -37,36 +37,51 @@ def calculate_drag(drag_coefficient, density, velocity, cross_sec_area):
     return drag
 
 def calculate_acceleration(thrust, drag, mass, g):
+    """Calculates the acceleration of the flight simulation."""
     acceleration = (thrust + drag + mass * g) / mass
     return acceleration
 
 def update_velocity(velocity, acceleration, dt):
+    """Updates the velocity of the flight simulation."""
     new_velocity = velocity + acceleration * dt
     return new_velocity
 
 def update_altitude(altitude, new_velocity, dt):
+    """Updates the altitude of the flight simulation."""
     new_altitude = altitude + new_velocity * dt
     return new_altitude
 
-#While loop calculating altitude
-while altitude >= 0:
-    thrust = calculate_thrust(time, burn_time)
-    density = calculate_density(density_sea_level, altitude, atmospheric_scale_height)
-    drag = calculate_drag(drag_coefficient, density, velocity, cross_sec_area)
-    acceleration = calculate_acceleration(thrust, drag, mass, g)
-    velocity = update_velocity(velocity, acceleration, dt)
-    altitude = update_altitude(altitude, velocity, dt)
+def simulate_flight(velocity, altitude, time, mass, dt, burn_time, g, drag_coefficient, cross_sec_area, density_sea_level, atmospheric_scale_height):
+    """Runs the flight simulation loop and returns (altitude_list, time_list)."""
+    altitude_list = []
+    time_list = []
+    while altitude >= 0:
+        thrust = calculate_thrust(time, burn_time)
+        density = calculate_density(density_sea_level, altitude, atmospheric_scale_height)
+        drag = calculate_drag(drag_coefficient, density, velocity, cross_sec_area)
+        acceleration = calculate_acceleration(thrust, drag, mass, g)
+        velocity = update_velocity(velocity, acceleration, dt)
+        altitude = update_altitude(altitude, velocity, dt)
+        time = time + dt
+        altitude_list.append(altitude)
+        time_list.append(time)
+    return time_list, altitude_list
 
-    time = time + dt
-    print(altitude)
-    altitude_list.append(altitude)
-    time_list.append(time)
+def plot_results(time_list, altitude_list):
+    """Plots the results of the flight simulation."""
+    plt.plot(time_list, altitude_list)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Altitude (m)")
+    plt.show()
 
-#Plotting the Altitudes and Time
-plt.plot(time_list, altitude_list)
-plt.xlabel("Time (s)")
-plt.ylabel("Altitude (m)")
-plt.show()
+def main():
+    """Runs the flight simulation loop."""
+    time_list, altitude_list = simulate_flight(velocity, altitude, time, mass, dt, burn_time, g, drag_coefficient, cross_sec_area,
+                    density_sea_level, atmospheric_scale_height)
+    plot_results(time_list, altitude_list)
+
+if __name__ == "__main__":
+    main()
 
 
 
