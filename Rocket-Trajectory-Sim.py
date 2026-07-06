b7085939d75d012e360d392c580089ad9b5193fa
Sam Rohrer <samueljrohrer@gmail.com>
2026-07-06 14:44:50 -0400

    Step D: Simple Atmospheric Model Added

--- Rocket-Trajectory-Sim.py
+++ Rocket-Trajectory-Sim.py
@@ -2,33 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Initial Constants
-velocity = 20
-altitude = 0
-time = 0
-mass = 34
-dt = 0.1
-g = -9.81
-drag_coefficient = 0.5  # Cd
-cross_sec_area = 0.0193  # m^2
-density_rho = 1.225  # kg/m^3
+velocity = 20 #m/s
+altitude = 0 #m
+time = 0 #s
+mass = 34 #kg
+dt = 0.1 #s
+g = -9.81 #m/s/s
+burn_time = 3.9 #s
+drag_coefficient = 0.5  #Cd
+cross_sec_area = 0.0193  #m^2
+density_sea_level = 1.225  #kg/m^3
+atmospheric_scale_height = 8500 #m
 #Lists of Altitudes and Times for every step
 altitude_list = []
 time_list = []
 
 #While loop calculating altitude
 while altitude >= 0:
-    burn_time = 3.9
     #If statement determining whether thrust is on or off
     if time < burn_time:
         thrust = 2500
     else:
         thrust = 0
+
+    #Exponential Atmospheric Model based on Isothermal Atmosphere
+    density_current = density_sea_level * np.exp(-altitude/atmospheric_scale_height)
+
     #Drag equation added as a force in acceleration
-    drag_equation = drag_coefficient * density_rho * velocity ** 2 / 2 * cross_sec_area
+    drag_equation = drag_coefficient * density_current * velocity ** 2 / 2 * cross_sec_area
+    #If statement deciding which direction drag force acts
     if velocity > 0:
         drag_equation = -drag_equation
     else:
         drag_equation = +drag_equation
+
     acceleration = (thrust + drag_equation + mass*g)/mass
     new_velocity = velocity + acceleration * dt
     new_altitude = altitude + new_velocity * dt
